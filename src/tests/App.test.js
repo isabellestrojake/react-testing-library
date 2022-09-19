e648b49ee567d9250c690a29d05803d5c2ad671d
Isabelle Strojake <strojake@gmail.com>
2022-09-19 19:13:47 -0300

    tests for app component

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,62 @@
-test('', () => {});
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import { act, screen } from '@testing-library/react';
+import renderWithRouter from '../Router/index';
+import App from '../App';
+
+describe('Teste se no componente App:', () => {
+  it('No topo da aplicação há um conjunto de links de navegação', () => {
+    renderWithRouter(<App />);
+
+    const homeLink = screen.getByRole('link', { name: /Home/i });
+    const aboutLink = screen.getByRole('link', { name: /About/i });
+    const favoriteLink = screen.getByRole('link', { name: /Favorite Pokémons/i });
+
+    expect(homeLink).toBeInTheDocument();
+    expect(aboutLink).toBeInTheDocument();
+    expect(favoriteLink).toBeInTheDocument();
+  });
+
+  it('Há um link para a Home', () => {
+    const { history } = renderWithRouter(<App />);
+    const homeLink = screen.getByRole('link', { name: /Home/i });
+
+    userEvent.click(homeLink);
+
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+
+  it('Há um link para o About', () => {
+    const { history } = renderWithRouter(<App />);
+    const aboutLink = screen.getByRole('link', { name: /About/i });
+
+    userEvent.click(aboutLink);
+
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  });
+
+  it('Há um link para os Favoritos', () => {
+    const { history } = renderWithRouter(<App />);
+    const favoriteLink = screen.getByRole('link', { name: /Favorite Pokémons/i });
+
+    userEvent.click(favoriteLink);
+
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+  });
+
+  it('Caso clique em uma url desconhecida deve renderizar a página Not Found', () => {
+    const { history } = renderWithRouter(<App />);
+    act(() => {
+      history.push('/wrong-page');
+    });
+
+    const notFound = screen.getByRole('heading', {
+      name: /Page requested not found/i,
+      level: 2,
+    });
+    expect(notFound).toBeInTheDocument();
+  });
+});
