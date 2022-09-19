8a85ffc5291a949695de963829837e4b0e273860
Isabelle Strojake <strojake@gmail.com>
2022-09-19 19:27:25 -0300

    tests for not found component

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../Router/index';
+import { NotFound } from '../pages';
+
+describe('Teste se o componente Not Found:', () => {
+  it('Contém um título de texto "Page requested not found"', () => {
+    renderWithRouter(<NotFound />);
+
+    const title = screen.getByRole('heading', { name: /Page requested not found/i });
+
+    expect(title).toBeInTheDocument();
+  });
+
+  it('Contém a imagem correta', () => {
+    renderWithRouter(<NotFound />);
+
+    const image = screen.getByRole('img');
+
+    expect(image).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
