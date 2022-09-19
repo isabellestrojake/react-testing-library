c55418f246f258b30e84ea1d70b3cd894ab15532
Isabelle Strojake <strojake@gmail.com>
2022-09-19 19:22:32 -0300

    tests for favorite pokemons component

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../Router/index';
+import { FavoritePokemons } from '../pages';
+import data from '../data';
+
+describe('Teste se no componente Favorite Pokémons', () => {
+  it('É exibida na tela a mensagem "No favorite Pokémon found" se não houver', () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const p = screen.getByText(/No favorite pokemon found/i);
+
+    expect(p).toBeInTheDocument();
+  });
+
+  it('É exibido os cards de todos os favoritos', () => {
+    renderWithRouter(<FavoritePokemons pokemons={ [data[0], data[1]] } />);
+
+    const pokemons = screen.getAllByTestId('pokemon-name');
+
+    expect(pokemons).toHaveLength(2);
+  });
+});
